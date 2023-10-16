82c80250180bcdbba10bbc5f2797a87e9b240b89
Kirill Darenskii <kdarenskii@mail.ru>
2023-10-16 16:30:45 +0300

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata, NextPage } from "next";
+
+export const metadata: Metadata = {
+    title: "HomePage | MyTrail",
+};
+
+const HomePage: NextPage = () => {
+    return <div></div>;
+};
+
+export default HomePage;
